fb445ab9abc96579d9734cbe573b6b159361e67b
FallenApart <slawomirkapka@gmail.com>
2020-11-18 23:51:20 +0100

    env finished

--- env.py
+++ env.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class Env:
     def __init__(self, nb_rows=4, nb_cols=4):
+        self.nb_actions = 4
         self.nb_rows, self.nb_cols = nb_rows, nb_cols
         self.reset()
         self.step_done = False
@@ -17,19 +18,21 @@ class Env:
         print(self.state)
 
     def step(self, action):
-        while not self.step_done:
-            if action == 0:
-                self.move_right()
-            elif action == 1:
-                self.move_left()
-            elif action == 2:
-                self.move_up()
-            elif action == 3:
-                self.move_down()
-
-        self.update_empty_cells()
-        self.add_number()
-        self.step_done = False
+        state = np.copy(self.state)
+        if action == 0:
+            self.move_right()
+        elif action == 1:
+            self.move_left()
+        elif action == 2:
+            self.move_up()
+        elif action == 3:
+            self.move_down()
+        if np.array_equal(state, self.state):
+            self.reward = - 2048  # The number is arbitrarily large
+        else:
+            self.update_empty_cells()
+            self.add_number()
+        self.is_done()
         return self.state, self.reward, self.done, None
 
     def update_empty_cells(self):
@@ -43,29 +46,25 @@ class Env:
         self.update_empty_cells()
 
     @staticmethod
-    def slide(row):
+    def slide_row_right(row):
         non_empty = row[row!=0]
         missing = row.shape[0] - non_empty.shape[0]
         new_row = np.append(np.zeros(missing), non_empty)
         return new_row
 
-    def move(self, row):
-        row = self.slide(row)
+    def move_row_right(self, row):
+        row = self.slide_row_right(row)
         for col in reversed(range(1, self.nb_cols)):
             if row[col] == row[col-1]:
                 self.reward = self.reward + row[col]
                 row[col] = 2 * row[col]
                 row[col-1] = 0
-        row = self.slide(row)
+        row = self.slide_row_right(row)
         return row
 
     def move_right(self):
-        new_state = np.copy(self.state)
         for row in range(self.nb_rows):
-            new_state[row] = self.move(self.state[row])
-        if not np.array_equal(new_state, self.state):
-            self.state = new_state
-            self.step_done = True
+            self.state[row] = self.move_row_right(self.state[row])
 
     def move_left(self):
         self.state = np.flip(self.state, axis=1)
@@ -86,12 +85,12 @@ class Env:
 
     def is_done(self):
         if np.sum(self.empty_cells) == 0:
-            for row in enumerate(self.nb_rows):
-                for col in enumerate(self.nb_cols):
+            for row in range(self.nb_rows):
+                for col in range(self.nb_cols):
                     if col != self.nb_cols-1 and self.state[row][col] == self.state[row][col+1]:
-                        pass
+                        return None
                     if row != self.nb_rows-1 and self.state[row][col] == self.state[row+1][col]:
-                        pass
+                        return None
             self.done = True
 
 
@@ -101,7 +100,7 @@ if __name__ == '__main__':
     step = 0
     while not env.done:
         action = np.random.randint(0, 4, size=1)[0]
-        new_state, _, _, _ = env.step(action)
+        new_state, reward, done, info = env.step(action)
         step += 1
         print('Step: {}'.format(step))
         print(new_state)
