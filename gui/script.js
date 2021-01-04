6a08592cc2984075cc3b901bb77bff299ff1f355
FallenApart <slawomirkapka@gmail.com>
2021-01-04 03:01:18 +0100

    gui for desplaying trajectoreis implemented

---
+++ gui/script.js
@@ -0,0 +1,63 @@
+var trajectory_json;
+var trajectory_length;
+
+function updateGrid(flat_grid, reward) {
+    this.game_score += reward
+    $("#score").text("Score: " + this.game_score);
+    $('.cell-value').each(function(cell, obj) {
+        let cell_value = flat_grid[cell];
+        if (cell_value == 0) {
+            $(this).text("");
+            $(this).parent().css({'background-color': '#e6f0ff'});
+        } else {
+            $(this).text(flat_grid[cell]);
+            let color_lightness = 50 / (Math.log2(flat_grid[cell]) + 1) + 50;
+            let hsl_color = 'hsl(255, 100%, ' + color_lightness.toString() + '%)';
+            $(this).parent().css({'background-color': hsl_color});
+        }
+    })
+}
+
+$(document).ready(function() {
+
+    let intervalId = false;
+    let flat_grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let done = false;
+    let game_score = 0;
+
+    $.getJSON("trajectory_0.json", function(json){trajectory_json = json;});
+
+    $('#run').click(() => {
+        run()
+    });
+
+    function run() {
+        done = false;
+        this.game_score = 0
+        trajectory_length = trajectory_json['actions'].length;
+        let step = 0;
+        if (intervalId) {
+            clearInterval(intervalId);
+            intervalId = false;
+        }
+        intervalId = setInterval(function(){
+            if (done) {
+                clearInterval(intervalId);
+                intervalId = false;
+                $("#status").text("Game Over");
+                return;
+            }
+            if (step == 0) {
+                updateGrid(trajectory_json['states'][step], 0);
+            } else if (0 < step && step < trajectory_length) {
+                console.log('active')
+                updateGrid(trajectory_json['states'][step], trajectory_json['rewards'][step-1]);
+            } else if (step == trajectory_length) {
+                updateGrid(trajectory_json['terminal_state'], trajectory_json['rewards'][step]);
+                done = true;
+            }
+            step++;
+        }, 10)
+    }
+
+})
